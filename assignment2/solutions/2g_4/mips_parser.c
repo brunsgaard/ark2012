db1af9b59c7ae95b83b569a0ccd27ecbba23e318
Martin Bjerregaard Jepsen <martin@jepsen.no>
2012-10-04 13:09:21 +0200

    Added meta instructions

--- assignment2/solutions/2g_4/mips_parser.c
+++ assignment2/solutions/2g_4/mips_parser.c
@@ -27,6 +27,29 @@ void parse_file (const char *filename)
 
 void parse_line (const char *line)
 {
+    // Add labels to label list
+    // Add code to code list
+    // Run meta-instructions
+}
+
+void run_meta (const char *instr)
+{
+    // .space
+    if (strncmp(instr, ".space", 6) == 0)
+    {
+
+    }
+
+    // .asciiz
+    if (strncmp(instr, ".asciiz", 7) == 0)
+    {
+
+    }
+}
+
+void run_instr (const char *instr)
+{
+
 }
 
 char *parse_labels (char *line)
