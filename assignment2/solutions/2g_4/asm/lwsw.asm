fe2b7b09e71a4987bb4e1b0e0515ba66b91b141c
Rasmus Wriedt Larsen <rasmuswriedtlarsen@gmail.com>
2012-10-07 17:49:17 +0200

    omproved lwsw test

--- assignment2/solutions/2g_4/asm/lwsw.asm
+++ assignment2/solutions/2g_4/asm/lwsw.asm
@@ -1,8 +1,13 @@
 start:
 	sub $t0, $0, 123
-	sub $sp, $sp, 4
+	sub $sp, $sp, 40
 	sw $t0, 0($sp)
 	lw $a0, 0($sp)
+	add $v0, $zero, 1
+	syscall
+	add $t0, $0, 123
+	sw $t0, 4($sp)
+	lw $a0, 4($sp)
 	add $v0, $zero, 1
 	syscall
 	and $a0, $zero, $zero
